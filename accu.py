4abf25dff46731b969b5cdc20b56b313516cedaf
Sasikala Mani <mani.37@ruan.cse.ohio-state.edu>
2017-08-01 14:40:08 -0400

    train accuracy

--- accu.py
+++ accu.py
@@ -4,8 +4,8 @@ import numpy
 
 (ranks, output) = MLP.test_mlp()
 ranks = ranks[0]
-print(len(ranks))
-print(len(ranks[0]))
+#print(len(ranks))
+#print(len(ranks[0]))
 
 
 accu = 0
@@ -17,7 +17,7 @@ for i in range(len(output)):
             accu = accu + (1 / float(103-count))
             break
         count += 1
-print(accu)
+print(accu/float(len(ranks)))
 
 
 
